10bda0d0fa301e00b6ccd8d498342afb2ce871bd
r1zwan07 <150995285+R1zwan07@users.noreply.github.com>
2026-02-04 14:31:22 +0530

    Add files via upload

---
+++ Day-3/immutable/backend.py
@@ -0,0 +1,6 @@
+# Tuple Immutability Demo 
+screen_res = (1920, 1080)
+
+print("Current Resolution: " + str(screen_res[0]) + "x" + str(screen_res[1]))
+
+print("Tuples cannot be modified! They are immutable.")
---
+++ Day-3/inventory_manager/backend.py
@@ -0,0 +1,14 @@
+# Simple Grocery Inventory 
+inventory = ["Apples", "Bananas", "Carrots", "Dates"]
+
+print("Current inventory:", inventory)
+
+inventory.append("Eggs")
+
+print("After adding Eggs:", inventory)
+
+inventory.remove("Bananas")
+print("After removing Bananas:", inventory)
+
+inventory.sort()
+print("Final sorted inventory:", inventory)
