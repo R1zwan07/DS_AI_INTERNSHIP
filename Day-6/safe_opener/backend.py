8461012caff0af31fc36cbbe9899c6703e2c5ef1
r1zwan07 <150995285+R1zwan07@users.noreply.github.com>
2026-02-07 12:44:01 +0530

    Add files via upload

---
+++ Day-6/safe_opener/backend.py
@@ -0,0 +1,11 @@
+filename = input("Enter the filename to open: ")
+
+try:
+    with open(filename, "r") as file:
+        content = file.read()
+        print("\nFile content:\n")
+        print(content)
+
+except FileNotFoundError:
+    print("\nOops! That file doesn't exist yet.")
+
